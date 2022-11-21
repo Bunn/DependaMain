bac14fdd1221c3be4238daa1088cfc3c6fcd7496
Fernando Bunn <bunn@idevzilla.com>
2022-11-21 20:10:30 +0000

    first commit

---
+++ TestDependa/TestDependa/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestDependa
+//
+//  Created by Fernando Bunn on 21/11/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
